6be4cc0c6aae6a321f1e1eac2e781589cadb7225
anu-shka11 <159987923+anu-shka11@users.noreply.github.com>
2024-09-24 17:45:14 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
 
-    const url = "http://localhost:4000"
+    const url = "https://food-del1-backend-pqyh.onrender.com"
 
     const [token , setToken] = useState("")
 
